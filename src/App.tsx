8822f5c27ae3d55f65da5adb957cbadc92a06030
JustBecerra <justj.becerra@gmail.com>
2025-01-25 19:25:54 -0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,6 @@ function App() {
 				<p className="apptitle">File Converter</p>
 			</div>
 			<div className="card">
-				<div className="fileinput">
-					<input type="file" />
-				</div>
-
 				<div className="selectcontainer">
 					<select name="original file">
 						<option>.epub</option>
@@ -20,6 +16,15 @@ function App() {
 						<option>.mobi</option>
 						<option>.epub</option>
 					</select>
+				</div>
+
+				<div className="fileinput">
+					<label htmlFor="dropFile" className="text-red-500">
+						Drag your file here or click in this area to select a
+						file
+					</label>
+
+					<input id="dropFile" type="file" />
 				</div>
 			</div>
 			<p className="read-the-docs">
