4723a44249c8d6a30c13c9cae86944674b340a91
JustBecerra <justj.becerra@gmail.com>
2025-02-24 17:18:44 -0300

    add styles for file already attached

--- src/App.tsx
+++ src/App.tsx
@@ -18,48 +18,68 @@ function App() {
 				<p className="text-[2em] mb-[1em] text-[#3bb5e9]">
 					File Converter
 				</p>
+				<p className="text-[1em]  text-white">
+					Drag and drop or click your file and select the format you
+					want to convert it to.
+				</p>
 			</div>
 			<div className="p-[2em] flex flex-col justify-center items-center h-[60%] w-full gap-[3em]">
-				<div className="flex justify-around items-center w-[80%]">
-					<select
-						className="bg-white text-black p-2 rounded w-[100px] h-[60px]"
-						name="original file"
-					>
-						<option>.epub</option>
-						<option>.mobi</option>
-					</select>
-					<p className="text-white text-xl">TO</p>
-					<select
-						className="bg-white text-black p-2 rounded w-[100px] h-[60px]"
-						name="new file"
-					>
-						<option>.mobi</option>
-						<option>.epub</option>
-					</select>
-				</div>
+				{droppedFile ? (
+					<div className="flex flex-col gap-6 items-center w-[80%]">
+						<div className="flex w-[100%] h-[60px]">
+							<div className="border-white border-1 border-solid w-[100%] h-[60px] flex justify-around items-center rounded">
+								<p className="text-white w-[70%] text-xl">
+									{droppedFile.name}
+								</p>
+								<p className="text-white w-[30%] text-xl border-l-white border-l-2 border-solid">
+									{droppedFile.size} bytes
+								</p>
+							</div>
+							<button
+								onClick={() => setDroppedFile(null)}
+								className="w-[50px] h-[100%] cursor-pointer border-white hover:font-bold border-solid hover:border-y-1 hover:border-r-1"
+							>
+								X
+							</button>
+						</div>
 
-				<div
-					onDrop={handleDrop}
-					className="bg-[#3bb5e9] cursor-pointer w-[80%] h-[70px] flex justify-center items-center rounded"
-				>
-					<label
-						htmlFor="dropFile"
-						className="text-white cursor-pointer p-4"
+						<p className="text-white text-xl ">TO</p>
+
+						<div className="flex justify-around items-center w-[100%]">
+							<select
+								className="bg-white text-black p-2 rounded w-[100px] h-[60px]"
+								name="new file"
+							>
+								<option>.mobi</option>
+								<option>.epub</option>
+								<option>.jpg</option>
+								<option>.png</option>
+							</select>
+							<button className="w-[100px] h-[60px] text-white bg-[#3bb5e9] cursor-pointer rounded">
+								Convert
+							</button>
+						</div>
+					</div>
+				) : (
+					<div
+						onDrop={handleDrop}
+						className="bg-[#3bb5e9] cursor-pointer w-[80%] h-[70px] flex justify-center items-center rounded"
 					>
-						{droppedFile ? (
-							<p>{droppedFile.name}</p>
-						) : (
+						<label
+							htmlFor="dropFile"
+							className="text-white cursor-pointer p-4"
+						>
 							<p>Drag your file or click here to upload a file</p>
-						)}
 
-						<input
-							id="dropFile"
-							onChange={handleFileChange}
-							type="file"
-							className="hidden"
-						/>
-					</label>
-				</div>
+							<input
+								id="dropFile"
+								onChange={handleFileChange}
+								type="file"
+								className="hidden"
+							/>
+						</label>
+					</div>
+				)}
 			</div>
 		</div>
 	)
